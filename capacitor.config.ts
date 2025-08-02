71dbbf29dcf98f28e01ec07ff9e1e36f3371082c
Ahsan Mahmood <aoneahsan@gmail.com>
2025-08-02 21:14:34 +0500

    working on package

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ahsanmahmood.cap_firebase',
-  appName: 'capacitor-firebase-kit',
-  webDir: 'dist',
-};
-
-export default config;
